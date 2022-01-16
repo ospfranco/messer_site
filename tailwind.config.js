2494f58fcc03f3a2d79f8a9df04f556b6f7ed956
Oscar Franco <ospfranco@protonmail.com>
2022-01-16 09:07:42 +0100

    Facelift

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,12 @@
-var colors = require("tailwindcss/colors");
-
-delete colors.lightBlue;
-
 module.exports = {
-  purge: [
+  content: [
     "./_includes/**/*.html",
     "./_layouts/**/*.html",
     "./_posts/*.md",
     "./*.html",
   ],
-  darkMode: "media",
   theme: {
     extend: {
-      colors,
-      width: {
-        768: "768px",
-      },
       keyframes: {
         fadein: {
           "0%": {
@@ -31,6 +22,4 @@ module.exports = {
       },
     },
   },
-  variants: {},
-  plugins: [require("tailwind-scrollbar-hide")],
 };
