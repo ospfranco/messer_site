44ccbe387c04cedd56e5614b79828dd5881a7ebd
Oscar Franco <ospfranco@protonmail.com>
2022-02-03 19:26:03 +0100

    Re-design

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -5,21 +5,5 @@ module.exports = {
     "./_posts/*.md",
     "./*.html",
   ],
-  theme: {
-    extend: {
-      keyframes: {
-        fadein: {
-          "0%": {
-            opacity: "0",
-          },
-          "100%": {
-            opacity: "1",
-          },
-        },
-      },
-      animation: {
-        fadein: "fadein 1.5s ease-in-out",
-      },
-    },
-  },
+  theme: {},
 };
